65a3b016c08e1de83596c82fd065908108740f37
salehin <nsalehin@gmail.com>
2017-08-19 11:48:59 -0700

    Initial Commit

---
+++ TipCalCodePath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalCodePath
+//
+//  Created by Salehin,Mohammad on 8/19/17.
+//  Copyright © 2017 Salehin,Mohammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
